084d1b30da7db27c4ef66cae2366c470d3d81155
RamilRakaev <59777361+RamilRakaev@users.noreply.github.com>
2022-09-20 02:37:43 +0300

    Created QuestionValidator.

---
+++ Questionnaire.Infrastructure/Queries/Handlers/Structures/GetNameIdFromStructuresHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Questionnaire.Domain.Entities;
+using Questionnaire.Domain.Interfaces;
+using Questionnaire.Infrastructure.Queries.Requests.Structures;
+
+namespace Questionnaire.Infrastructure.Queries.Handlers.Structures
+{
+    public class GetNameIdFromStructuresHandler : IRequestHandler<GetNameIdFromStructuresQuery, Dictionary<string, int>>
+    {
+        private readonly IRepository<Structure> _structureRepository;
+
+        public GetNameIdFromStructuresHandler(IRepository<Structure> structureRepository)
+        {
+            _structureRepository = structureRepository;
+        }
+
+        public Task<Dictionary<string, int>> Handle(GetNameIdFromStructuresQuery request, CancellationToken cancellationToken)
+        {
+            return _structureRepository.GetAllAsNoTracking()
+                .ToDictionaryAsync(structure => structure.DisplayName, structure => structure.Id, cancellationToken);
+        }
+    }
+}
---
+++ Questionnaire.Infrastructure/Queries/Requests/Structures/GetNameIdFromStructuresQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Questionnaire.Infrastructure.Queries.Requests.Structures
+{
+    public class GetNameIdFromStructuresQuery : IRequest<Dictionary<string, int>>
+    {
+    }
+}
