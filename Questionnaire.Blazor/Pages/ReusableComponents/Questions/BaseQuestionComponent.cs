9faf4237009c206a6ee2fd463d17319b87bade20
Рамиль Ракаев <ramrg546@gmail.com>
2022-10-04 15:48:26 +0300

    WIP: components for displaying questions.

---
+++ Questionnaire.Blazor/Pages/ReusableComponents/Questions/BaseQuestionComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace Questionnaire.Blazor.Pages.ReusableComponents.Questions
+{
+    public abstract class BaseQuestionComponent<T> : ComponentBase
+    {
+        [Parameter]
+        public string Label { get; set; }
+
+        [Parameter]
+        public T Value { get; set; }
+
+        public EventCallback<T> ValueChanged { get; set; }
+
+        protected async Task SetValue(ChangeEventArgs eventArgs)
+        {
+            if (TryParse(eventArgs.Value.ToString(), out T newValue))
+            {
+                Value = newValue;
+                await ValueChanged.InvokeAsync(newValue);
+            }
+        }
+
+        protected abstract bool TryParse(string original, out T result);
+    }
+}
